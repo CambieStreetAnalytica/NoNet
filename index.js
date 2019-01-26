822a56f472d47ee56ed3d48944d0ea599d513b5b
Declan Herbertson <dherbertson@alumni.ubc.ca>
2019-01-26 14:17:49 -0800

    test response

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => res.send('Application Base'));
+app.get('/rest/helloWorld', (req, res) => {
+    res.send("hello back!!!");
+});
 
-app.listen(port, () => console.log(`Example app listening on port 
-${port}!`))
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+app.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+
+    twiml.message('The Robots are coming! Head for the hills!');
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
