0e4954587435c3d3f3826b68fcddc2510f560f50
Declan Herbertson <dherbertson@alumni.ubc.ca>
2019-01-26 22:20:32 -0800

    changing format of search

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ const wiki_key = "002412142127618762442:q3eq1hoh6wu";
 const goog_key = "016717304083729390418:3pmwddke6q4";
 const api_key = "AIzaSyCeb1PbN_a3Bth_f6VR9krKRjKuc1KPcjw";
 
+const DEFAULT_SEARCH_LIMIT = 5;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
+  console.log(parseMessage("yelp: chinese"));
+  console.log(parseMessage("yelp 10: chinese"));
+  console.log(parseMessage("hello world"));
 });
 app.get("/", (req, res) => {
   res.send("Application Base");
@@ -88,11 +93,11 @@ function search(query, amount, key, parsing) {
 
 //parses the sent text message into {type, amount, query} -- returns null if msg is malformed
 function parseMessage(msg) {
-  // msg will look like 'wiki 10 germany' or 'yelp 5 chinese food'
-  const sections = msg.split(" ");
-  const type = sections[0];
-  const amount = Number(sections[1]);
-  const query = sections.splice(2).join(" ");
+    // msg will look like 'wiki 10: germany' or 'wiki: germany' or 'yelp 5: chinese food' or 'yelp: chinese'
+    const prefixes = msg.split(":")[0].split(" ");
+    const type = prefixes[0];
+    const amount = prefixes.length > 1 ? Number(prefixes[1]) : DEFAULT_SEARCH_LIMIT;
+    const query = msg.split(":").length > 1 ? msg.split(":")[1].trim() : undefined;
 
   const isValid =
     validTypes.includes(type) && !Number.isNaN(amount) && query !== undefined;
