33ce96cfddc550463dd2e154f8d6422ca462ef53
Declan Herbertson <dherbertson@alumni.ubc.ca>
2019-01-27 12:22:49 -0800

    fixing cmd

--- index.js
+++ index.js
@@ -325,19 +325,28 @@ function parseURL(res) {
     }
 }
 
-const webHelp = "WEB AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Google, AMOUNT has a maximum of 10 and a default of 5";
-
-const yelpHelp = "Yelp AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Yelp, AMOUNT has a maximum of 10 and a default of 5";
-
-const wikiHelp = "Wiki AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Wikipedia, AMOUNT has a maximum of 10 and a default of 5";
-
-const urlHelp = "Url: URL - Returns important text from URL (article body, etc), do not include https:// or https:// in URL";
-
-const trHelp = "Tr LANG: TEXT_TO_TRANSLATE - Translates TEXT_TO_TRANSLATE in to LANG - supported LANG values: en, es, fr, de, ru";
-
-const notFound = "This command is not recognized";
-
 function searchHelp(query) {
+    const genericHelp = `These are the possible commands available \n
+        Web - Search Google\n
+        Yelp - Search Yelp\n
+        Wiki - Search Wikipedia\n
+        Url - get Website text\n
+        Tr - get translation\n
+        commands are not case sensitive \n
+        for more information, please type 'help: COMMAND'`;
+
+    const webHelp = "WEB AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Google, AMOUNT has a maximum of 10 and a default of 5";
+
+    const yelpHelp = "Yelp AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Yelp, AMOUNT has a maximum of 10 and a default of 5";
+
+    const wikiHelp = "Wiki AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Wikipedia, AMOUNT has a maximum of 10 and a default of 5";
+
+    const urlHelp = "Url: URL - Returns important text from URL (article body, etc), do not include https:// or https:// in URL";
+
+    const trHelp = "Tr LANG: TEXT_TO_TRANSLATE - Translates TEXT_TO_TRANSLATE in to LANG - supported LANG values: en, es, fr, de, ru";
+
+    const notFound = "This command is not recognized";
+
     return new Promise(function (res, req) {
         if (query === undefined || query === null) {
             res(genericHelp);
