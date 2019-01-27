80db45fa9c2acf80501be5bf0a9690889b658e8e
Declan Herbertson <dherbertson@alumni.ubc.ca>
2019-01-27 12:26:25 -0800

    add spacing

--- index.js
+++ index.js
@@ -335,15 +335,15 @@ function searchHelp(query) {
         commands are not case sensitive \n
         for more information, please type 'help: COMMAND'`;
 
-    const webHelp = "WEB AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Google, AMOUNT has a maximum of 10 and a default of 5";
+    const webHelp = "\nWEB AMOUNT(optional): QUERY \n- gets top AMOUNT results of QUERY from Google, AMOUNT has a maximum of 10 and a default of 5";
 
-    const yelpHelp = "Yelp AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Yelp, AMOUNT has a maximum of 10 and a default of 5";
+    const yelpHelp = "\nYelp AMOUNT(optional): QUERY \n- gets top AMOUNT results of QUERY from Yelp, AMOUNT has a maximum of 10 and a default of 5";
 
-    const wikiHelp = "Wiki AMOUNT(optional): QUERY - gets top AMOUNT results of QUERY from Wikipedia, AMOUNT has a maximum of 10 and a default of 5";
+    const wikiHelp = "\nWiki AMOUNT(optional): QUERY \n- gets top AMOUNT results of QUERY from Wikipedia, AMOUNT has a maximum of 10 and a default of 5";
 
-    const urlHelp = "Url: URL - Returns important text from URL (article body, etc), do not include https:// or https:// in URL";
+    const urlHelp = "\nUrl: URL \n- Returns important text from URL (article body, etc), do not include https:// or https:// in URL";
 
-    const trHelp = "Tr LANG: TEXT_TO_TRANSLATE - Translates TEXT_TO_TRANSLATE in to LANG - supported LANG values: en, es, fr, de, ru";
+    const trHelp = "\nTr LANG: TEXT_TO_TRANSLATE - Translates TEXT_TO_TRANSLATE in to LANG \n- supported LANG values: en, es, fr, de, ru";
 
     const notFound = "This command is not recognized";
 
